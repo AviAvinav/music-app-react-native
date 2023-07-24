eaa730a378fe685e45bfa0d2564acfb95b3fb5d8
Avi Avinav <aviavinav2004@gmail.com>
2023-07-24 22:16:38 +0530

    make basic header might add styled-components

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,20 +1,71 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import Avatar from "../components/Avatar";
+import { useFonts } from "expo-font";
+import { useCallback } from "react";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
+
+const data = [
+  { id: 1, value: "Indie" },
+  { id: 2, value: "Chill" },
+  { id: 3, value: "Pop" },
+  { id: 4, value: "Party" },
+];
 
 export default function Home() {
+  const [fontsLoaded, err] = useFonts({
+    "CG-Bold": require("../assets/fonts/ClashGrotesk-Bold.otf"),
+  });
+
+  if (err) {
+    console.log(err);
+  }
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.profile}>
-        <Text style={{ fontSize: 30, fontWeight: "900" }}>Hi Austin!</Text>
-        <Avatar />
-      </View>
+    <SafeAreaView style={styles.container} onLayout={onLayoutRootView}>
+      <ScrollView>
+        <View style={styles.profile}>
+          <Text style={{ fontFamily: "CG-Bold", fontSize: 30 }}>
+            Hi Austin!
+          </Text>
+          <Avatar />
+        </View>
+        <FlatList
+          data={data}
+          renderItem={({ item }) => (
+            <View style={styles.category}>
+              <Text>{item.value}</Text>
+            </View>
+          )}
+          numColumns={2}
+          style={styles.categories}
+        ></FlatList>
+      </ScrollView>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 60,
+    paddingTop: 20,
     paddingHorizontal: 20,
     flex: 1,
   },
@@ -23,5 +74,13 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
+  },
+  categories: {
+    marginVertical: 20,
+    backgroundColor: "#fff",
+  },
+  category: {
+    width: "40%",
+    margin: 15,
   },
 });
